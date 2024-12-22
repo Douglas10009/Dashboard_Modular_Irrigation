d984eced7af9dadd3d8f07164965a2819423728b
Douglas <douglasevertonev@gmail.com>
2024-12-22 14:47:55 -0300

    feat: Finishing data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { dataResult } from '../models/dataResult';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,13 @@ export class DataService {
   apiURL:string = environment.apiURL;
 
   constructor(private http:HttpClient) { }
+
+  getDataByID(id:number){
+    return this.http.get<dataResult>(this.apiURL + id +"/")
+    .pipe(
+      map((response)=> {
+        return response;
+      })
+    )
+  }
 }
