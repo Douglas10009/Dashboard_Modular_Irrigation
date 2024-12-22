f74224bfd04a3829184435b6481144792ab1fee2
Douglas <douglasevertonev@gmail.com>
2024-12-22 20:46:35 -0300

    feat: Home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { DataCardComponent } from '../../components/data-card/data-card.component';
+import { DataService } from '../../services/data.service';
+import { dataResults } from '../../models/dataResult';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DataCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  data!: dataResults[];
 
+  device!: string;
+  createdAt!: string;
+  valueHuminity!: string;
+  valueTemperature!: string;
+  valueLuminocity!: string;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getData().subscribe((data)=> {
+      const lastIndex = data.length - 1;
+
+      this.device = data[lastIndex].dispositivo;
+      this.createdAt = data[lastIndex].createdAt;
+      this.createdAt = new Date(data[lastIndex].createdAt).toLocaleDateString('pt-BR');
+      this.valueHuminity = data[lastIndex].umidade;
+      this.valueTemperature = data[lastIndex].temperatura;
+      this.valueLuminocity = data[lastIndex].luminosidade;
+    })
+  }
 }
