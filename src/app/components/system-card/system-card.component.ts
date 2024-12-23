ad3bfb02ce7f6ee52ea3c4f7d363b1688b7c519a
Douglas <douglasevertonev@gmail.com>
2024-12-23 11:43:08 -0300

    feat: system card component

---
+++ src/app/components/system-card/system-card.component.ts
@@ -0,0 +1,65 @@
+import { Component, Input } from '@angular/core';
+import { dataResults } from '../../models/dataResult';
+import { DataService } from '../../services/data.service';
+import { DataCardComponent } from '../data-card/data-card.component';
+
+@Component({
+  selector: 'app-system-card',
+  standalone: true,
+  imports: [DataCardComponent],
+  templateUrl: './system-card.component.html',
+  styleUrl: './system-card.component.scss',
+})
+export class SystemCardComponent {
+  data!: dataResults[];
+
+  @Input() device!: string;
+  apiDevice!: string;
+
+  id!: number;
+  createdAt!: string;
+  valueHuminity!: string;
+  valueTemperature!: string;
+  valueLuminocity!: string;
+  valueBatteryCharge!: string;
+  statusBattery!: number;
+  isBatteryCharging!: string;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getData().subscribe((data) => {
+      // Percorrer por todos os dados da API
+      data.forEach((eachData) => {
+
+        // Somente pega os dados do dispositivo daquele sistema específico
+        if (eachData.dispositivo == this.device) {
+          this.id = eachData.id;
+          this.createdAt = eachData.createdAt;
+          this.createdAt = new Date(eachData.createdAt).toLocaleDateString(
+            'pt-BR'
+          );
+          this.valueHuminity = eachData.umidade;
+          this.valueTemperature = eachData.temperatura;
+          this.valueLuminocity = eachData.luminosidade;
+          this.valueBatteryCharge = eachData.cargaBateria + '%';
+          this.statusBattery = parseInt(eachData.statusBateria);
+          this.parseStatusBattery();
+        }
+      });
+    });
+  }
+
+  // TODO: Implemntar a função de enviar SMS
+  sendSMS() {
+    console.log('SMS enviado');
+  }
+
+  async parseStatusBattery() {
+    if (this.statusBattery == 1) {
+      this.isBatteryCharging = 'Charging';
+    } else if (this.statusBattery == 0) {
+      this.isBatteryCharging = 'Not Charging';
+    }
+  }
+}
