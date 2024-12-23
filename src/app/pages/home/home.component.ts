682481effa6be244e058b990cdbfa4f8f5e61dcc
Douglas <douglasevertonev@gmail.com>
2024-12-23 11:44:11 -0300

    fix: Modulation of info (created systemcardComponent)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,53 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DataCardComponent } from '../../components/data-card/data-card.component';
 import { DataService } from '../../services/data.service';
 import { dataResults } from '../../models/dataResult';
+import { SystemCardComponent } from '../../components/system-card/system-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DataCardComponent],
+  imports: [SystemCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  data!: dataResults[];
 
-  device!: string;
-  createdAt!: string;
-  valueHuminity!: string;
-  valueTemperature!: string;
-  valueLuminocity!: string;
-  valueBatteryCharge!: string;
-  statusBattery!: number;
-  isBatteryCharging!: string;
+  // Fazer isso com os devices únicos que são recebidos da API
+  @Input() deviceNumber1 = "86357604940642";
+  @Input() deviceNumber2 = "2060";
+  @Input() deviceNumber3 = "";
 
-
-
-  constructor(private dataService: DataService) { }
-
-  ngOnInit() {
-    this.dataService.getData().subscribe((data)=> {
-      const lastIndex = data.length - 1;
-
-      this.device = data[lastIndex].dispositivo;
-      this.createdAt = data[lastIndex].createdAt;
-      this.createdAt = new Date(data[lastIndex].createdAt).toLocaleDateString('pt-BR');
-      this.valueHuminity = data[lastIndex].umidade;
-      this.valueTemperature = data[lastIndex].temperatura;
-      this.valueLuminocity = data[lastIndex].luminosidade;
-      this.valueBatteryCharge = data[lastIndex].cargaBateria + "%";
-      this.statusBattery = parseInt(data[lastIndex].statusBateria);
-
-      if (this.statusBattery == 1) {
-        this.isBatteryCharging = "Charging";
-      } else if (this.statusBattery == 0) {
-        this.isBatteryCharging = "Not Charging";
-      }
-    })
-  }
-
-  sendSMS(){
-    console.log("SMS enviado");
-  }
 }
