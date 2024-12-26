bb24724f5f28762f6b8b0207056cdfec33486154
Douglas <douglasevertonev@gmail.com>
2024-12-26 19:42:00 -0300

    feat: Init details component

--- src/app/pages/details-system/details-system.component.ts
+++ src/app/pages/details-system/details-system.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { SystemCardComponent } from '../../components/system-card/system-card.component';
+import { DataCardComponent } from '../../components/data-card/data-card.component';
 
 @Component({
   selector: 'app-details-system',
   standalone: true,
-  imports: [SystemCardComponent],
+  imports: [DataCardComponent],
   templateUrl: './details-system.component.html',
   styleUrl: './details-system.component.scss'
 })
@@ -13,4 +13,10 @@ export class DetailsSystemComponent {
   device = this.activatedRoute.snapshot.params['device'];
 
   constructor (private activatedRoute: ActivatedRoute) {}
+
+  valueHuminity:string = '10';
+  valueTemperature:string = '10';
+  valueLuminocity:string = '10';
+  valueBatteryCharge:string = '10';
+  isBatteryCharging:string = '10';
 }
